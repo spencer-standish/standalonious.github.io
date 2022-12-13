d197b622a928e08c8f13547d65ac0a5cf69efc15
keziah <kezker1903@gmail.com>
2022-12-12 22:26:14 -0500

    final

--- script/quiz.js
+++ script/quiz.js
@@ -3,9 +3,9 @@ const Questions = [{
     id: 0,
     q: "What is the polar arctic fox's main food source?",
     a: [{ text: "fruits and vegetables", isCorrect: false, isSelected: false },
-        { text: "leftovers from polar bear kills", isCorrect: false },
-        { text: "lemmings", isCorrect: true },
-        { text: "whatever washes up on the seashore", isCorrect: false }
+        { text: "leftovers from polar bear kills", isCorrect: false, isSelected: false },
+        { text: "lemmings", isCorrect: true, isSelected: false },
+        { text: "whatever washes up on the seashore", isCorrect: false, isSelected: false }
     ]
 
 },
@@ -13,9 +13,9 @@ const Questions = [{
     id: 1,
     q: "What is a baby arctic hare called?",
     a: [{ text: "a cub", isCorrect: false, isSelected: false },
-        { text: "a kitten", isCorrect: false },
-        { text: "a baby", isCorrect: false },
-        { text: "a leveret", isCorrect: true }
+        { text: "a kitten", isCorrect: false, isSelected: false },
+        { text: "a baby", isCorrect: false, isSelected: false },
+        { text: "a leveret", isCorrect: true, isSelected: false }
     ]
 
 },
@@ -23,9 +23,9 @@ const Questions = [{
     id: 2,
     q: "A killer whale has a large _____ on its back.",
     a: [{ text: "Flipper", isCorrect: false, isSelected: false },
-        { text: "Dorsal fin", isCorrect: true },
-        { text: "Red spot", isCorrect: false },
-        { text: "Scar", isCorrect: false }
+        { text: "Dorsal fin", isCorrect: true, isSelected: false  },
+        { text: "Red spot", isCorrect: false, isSelected: false  },
+        { text: "Scar", isCorrect: false, isSelected: false  }
     ]
 
 },
@@ -33,9 +33,9 @@ const Questions = [{
     id: 3,
     q: "Beluga whales live in groups called what?",
     a: [{ text: "Flocks", isCorrect: false, isSelected: false },
-        { text: "Packs", isCorrect: false },
-        { text: "Litters", isCorrect: false },
-        { text: "Pods", isCorrect: true }
+        { text: "Packs", isCorrect: false, isSelected: false  },
+        { text: "Litters", isCorrect: false, isSelected: false  },
+        { text: "Pods", isCorrect: true, isSelected: false  }
     ]
 
 },
@@ -43,9 +43,9 @@ const Questions = [{
     id: 4,
     q: "Why don't adult brown bears climb trees?",
     a: [{ text: "Their claws aren't long enough.", isCorrect: false, isSelected: false },
-        { text: "They weigh too much.", isCorrect: true },
-        { text: "They are afraid of heights.", isCorrect: false },
-        { text: "They dont know how to climb", isCorrect: false }
+        { text: "They weigh too much.", isCorrect: true, isSelected: false  },
+        { text: "They are afraid of heights.", isCorrect: false, isSelected: false  },
+        { text: "They dont know how to climb", isCorrect: false, isSelected: false  }
     ]
 
 },
@@ -53,9 +53,9 @@ const Questions = [{
     id: 5,
     q: "How fast can a moose run on land (over a short distance)?",
     a: [{ text: "Up to 35 miles an hour", isCorrect: true, isSelected: false },
-        { text: "Up to 50 miles an hour", isCorrect: false },
-        { text: "Up to 5 miles an hour", isCorrect: false },
-        { text: "A moose can't run", isCorrect: false }
+        { text: "Up to 50 miles an hour", isCorrect: false, isSelected: false  },
+        { text: "Up to 5 miles an hour", isCorrect: false, isSelected: false  },
+        { text: "A moose can't run", isCorrect: false, isSelected: false  }
     ]
 
 },
@@ -63,9 +63,9 @@ const Questions = [{
     id: 6,
     q: "How long do alpacas live on an average?",
     a: [{ text: "15 years", isCorrect: true, isSelected: false },
-        { text: "25 years", isCorrect: false },
-        { text: "10 years", isCorrect: false },
-        { text: "20 years", isCorrect: false }
+        { text: "25 years", isCorrect: false, isSelected: false  },
+        { text: "10 years", isCorrect: false, isSelected: false  },
+        { text: "20 years", isCorrect: false, isSelected: false  }
     ]
 
 },
@@ -73,9 +73,9 @@ const Questions = [{
     id: 7,
     q: "A baby reindeer is called a _____.",
     a: [{ text: "Bull", isCorrect: false, isSelected: false },
-        { text: "Doe", isCorrect: false },
-        { text: "Cow", isCorrect: false },
-        { text: "Calf", isCorrect: true }
+        { text: "Doe", isCorrect: false, isSelected: false  },
+        { text: "Cow", isCorrect: false, isSelected: false  },
+        { text: "Calf", isCorrect: true, isSelected: false  }
     ]
 
 },
@@ -83,9 +83,9 @@ const Questions = [{
     id: 8,
     q: "Canadian lynx can spot prey at night upto _____ feet.",
     a: [{ text: "150", isCorrect: false, isSelected: false },
-        { text: "100", isCorrect: false },
-        { text: "50", isCorrect: false },
-        { text: "250", isCorrect: true }
+        { text: "100", isCorrect: false, isSelected: false  },
+        { text: "50", isCorrect: false, isSelected: false  },
+        { text: "250", isCorrect: true, isSelected: false  }
     ]
 
 },
@@ -93,9 +93,9 @@ const Questions = [{
     id: 9,
     q: "Why did American bison almost become extinct?",
     a: [{ text: "They starved.", isCorrect: false, isSelected: false },
-        { text: "People hunted them.", isCorrect: true },
-        { text: "They didn't have enough baby calves.", isCorrect: false },
-        { text: "The temperature got to hot for them to survive.", isCorrect: false }
+        { text: "People hunted them.", isCorrect: true, isSelected: false  },
+        { text: "They didn't have enough baby calves.", isCorrect: false, isSelected: false  },
+        { text: "The temperature got to hot for them to survive.", isCorrect: false, isSelected: false  }
     ]
 
 },
@@ -103,9 +103,9 @@ const Questions = [{
     id: 10,
     q: "In what type of water do beavers live?",
     a: [{ text: "Brackish", isCorrect: false, isSelected: false },
-        { text: "Fresh", isCorrect: false },
-        { text: "Salt", isCorrect: false },
-        { text: "Estuarine", isCorrect: true }
+        { text: "Fresh", isCorrect: false, isSelected: false  },
+        { text: "Salt", isCorrect: false, isSelected: false  },
+        { text: "Estuarine", isCorrect: true, isSelected: false  }
     ]
 
 },
@@ -113,9 +113,9 @@ const Questions = [{
     id: 11,
     q: "Where do polar bears live?",
     a: [{ text: "the Arctic", isCorrect: true, isSelected: false },
-        { text: "the Antarctic", isCorrect: false },
-        { text: "Asia", isCorrect: false },
-        { text: "Africa", isCorrect: true }
+        { text: "the Antarctic", isCorrect: false, isSelected: false  },
+        { text: "Asia", isCorrect: false, isSelected: false  },
+        { text: "Africa", isCorrect: true, isSelected: false  }
     ]
 
 }
@@ -208,6 +208,10 @@ evaluate[0].addEventListener("click", () => {
         result[0].innerHTML = "False";
         result[0].style.color = "red";
     }
+op1.style.backgroundColor = "lightskyblue";
+op2.style.backgroundColor = "lightskyblue";
+op3.style.backgroundColor = "lightskyblue";
+op4.style.backgroundColor = "lightskyblue";
 })
 }
 
@@ -226,5 +230,9 @@ if (id < 12) {
     iterate(id);
     console.log(id);
 }
+op1.style.backgroundColor = "lightskyblue";
+op2.style.backgroundColor = "lightskyblue";
+op3.style.backgroundColor = "lightskyblue";
+op4.style.backgroundColor = "lightskyblue";
 
 })
